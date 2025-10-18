c7cb9fc396d8010eac5b7dd0c76c62fc9376e387
Uma Menda <uma.menda@intownlocal.com>
2025-10-18 07:59:54 +0000

    auto-commit for cc185170-76df-4a8b-9ae2-bd20a8f24a5b

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout() {
       <Stack.Screen name="location" />
       <Stack.Screen name="login" />
       <Stack.Screen name="otp" />
+      <Stack.Screen name="user-dashboard" />
+      <Stack.Screen name="register-member" />
+      <Stack.Screen name="register-merchant" />
       <Stack.Screen name="dashboard" />
       <Stack.Screen name="map" />
       <Stack.Screen name="shop-details" />
